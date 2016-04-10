7e25827b141c2d9ed25ba470735c10f16ec05679
mattmcfad <matt.mcfadyen@rangle.io>
2016-04-10 02:24:02 -0400

    :no_pedestrians:

--- server/controllers/devController.js
+++ server/controllers/devController.js
@@ -7,25 +7,19 @@ const Dev = require('../models/dev');
 const controller = {};
 
 controller.get = function(req, res) {
-  console.log(yellow.bold(`Getting users`));
 
   Dev.find((err, devs) => {
     if (err) {
       console.error(red.bold(`${err} sorry!`));
-      res.status(500).send(`${err} sorry!`);
+      res.status(500);
     }
 
-    console.log(green.bold(`Found users: ${devs}`));
     res.status(200).json(devs);
   });
+
 };
 
 controller.post = function(req, res) {
-  console.log(yellow.bold(`Posting new users`));
-  console.log(yellow.bold(`
-    name: ${req.body.name}, age: ${req.body.age}
-    lang: ${req.body.lang} email: ${req.body.email}
-  `));
 
   const dev = new Dev({
     name: req.body.name,
@@ -40,9 +34,8 @@ controller.post = function(req, res) {
       res.status(500).send(`Mongo Error: ${err}`);
     }
 
-    console.log(green.bold(`Created Dev: ${newDev}`));
-
     res.status(200).json(newDev);
+    console.log(green.bold(`Created Dev: ${newDev}`));
   })
 
 };
